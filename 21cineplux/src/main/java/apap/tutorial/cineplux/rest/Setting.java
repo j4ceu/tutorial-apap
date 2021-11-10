80cb791e49f4703b8adaf11863d64f53c5b1c705
cdg1st <sutan.raihan@ui.ac.id>
2021-11-10 22:44:09 +0700

    feat:Latihan 1 dan 2

---
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/rest/Setting.java
@@ -0,0 +1,6 @@
+package apap.tutorial.cineplux.rest;
+
+public class Setting {
+    final public static String bioskopUrl = "https://7936306a-a412-4a61-9f92-e582c507be72.mock.pstmn.io";
+    final public static String umurUrl = "https://api.agify.io";
+}
---
+++ 21cineplux/src/main/java/apap/tutorial/cineplux/service/PenjagaRestServiceImpl.java
@@ -0,0 +1,115 @@
+package apap.tutorial.cineplux.service;
+
+import apap.tutorial.cineplux.model.BioskopModel;
+import apap.tutorial.cineplux.model.PenjagaModel;
+import apap.tutorial.cineplux.repository.BioskopDB;
+import apap.tutorial.cineplux.repository.PenjagaDB;
+import apap.tutorial.cineplux.rest.BioskopDetail;
+import apap.tutorial.cineplux.rest.Setting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import javax.transaction.NotSupportedException;
+import javax.transaction.Transactional;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@Transactional
+public class PenjagaRestServiceImpl implements PenjagaRestService {
+    private final WebClient webClient;
+
+    @Autowired
+    private PenjagaDB penjagaDB;
+
+    @Autowired
+    BioskopDB bioskopDB;
+
+    @Override
+    public PenjagaModel createPenjaga(PenjagaModel penjaga) {
+        return penjagaDB.save(penjaga);
+    }
+
+    @Override
+    public List<PenjagaModel> retrievePenjagaList() {
+        return penjagaDB.findAll();
+    }
+
+    @Override
+    public PenjagaModel getPenjagaByNoPenjaga(Long noPenjaga){
+        Optional<PenjagaModel> penjaga = penjagaDB.findByNoPenjaga(noPenjaga);
+        if(penjaga.isPresent()){
+            return penjaga.get();
+        }
+        throw  new NoSuchElementException();
+    }
+
+    @Override
+    public PenjagaModel updatePenjaga(Long noPenjaga, PenjagaModel penjagaUpdate) {
+        LocalTime now = LocalTime.now();
+        PenjagaModel penjaga = getPenjagaByNoPenjaga(noPenjaga);
+        BioskopModel bioskop = bioskopDB.findByNoBioskop(penjaga.getBioskop().getNoBioskop()).get();
+        if((now.isBefore(bioskop.getWaktuBuka()) || now.isAfter(bioskop.getWaktuTutup()))) {
+            penjaga.setNamaPenjaga(penjagaUpdate.getNamaPenjaga());
+            penjaga.setJenisKelamin(penjagaUpdate.getJenisKelamin());
+            Optional<BioskopModel> newBioskop = bioskopDB.findByNoBioskop(penjagaUpdate.getBioskop().getNoBioskop());
+            if (newBioskop.isPresent()){
+                penjaga.setBioskop(penjagaUpdate.getBioskop());
+            } else {
+                throw  new NoSuchElementException("No Bioskop Not Found!");
+            }
+
+            return penjagaDB.save(penjaga);
+        } else {
+            throw new UnsupportedOperationException("Bioskop is still open!");
+        }
+    }
+
+    @Override
+    public void deletePenjaga(Long noPenjaga){
+        LocalTime now = LocalTime.now();
+        PenjagaModel penjaga = getPenjagaByNoPenjaga(noPenjaga);
+        BioskopModel bioskop = bioskopDB.findByNoBioskop(penjaga.getBioskop().getNoBioskop()).get();
+        if((now.isBefore(bioskop.getWaktuBuka()) || now.isAfter(bioskop.getWaktuTutup()))) {
+            penjagaDB.delete(penjaga);
+        } else {
+            throw new UnsupportedOperationException("Bioskop is still open!");
+        }
+    }
+
+    public PenjagaRestServiceImpl(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl(Setting.umurUrl).build();
+    }
+
+    @Override
+    public PenjagaModel getUmur(Long noPenjaga){
+        PenjagaModel penjaga = getPenjagaByNoPenjaga(noPenjaga);
+        String namaPenjaga = penjaga.getNamaPenjaga();
+        String[] firstName = namaPenjaga.split(" ");
+
+        String getStringApi = this.webClient.get().uri("/?name=" + firstName[0])
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        getStringApi = getStringApi.replace("{", "");
+        getStringApi = getStringApi.replace("}", "");
+
+        String[] splitStringApi = getStringApi.split(",");
+        String[] getAge = splitStringApi[1].split(":");
+        System.out.println(getAge[1]);
+        penjaga.setUmur(getAge[1]);
+
+        return penjagaDB.save(penjaga);
+
+
+
+    }
+
+
+}
